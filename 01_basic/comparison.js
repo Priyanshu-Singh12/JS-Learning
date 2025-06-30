1e9de5311afc4613af3b3f15f4395be7981b0b94
Priyanshu-Singh12 <progcoder34@gmail.com>
2025-06-30 21:12:56 +0530

    c

---
+++ 01_basic/comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1)
+console.log("02">1)
+
+//avoid these type of compairson and make code as readable as possible
+
+
+//== and >= work differently
+console.log(null>0)
+console.log(null==0) //false not null chnges to NaN
+console.log(null<=0)  //true  here equal to 0(number)
+
+console.log(null==undefined) //true
+console.log(undefined==0)
+console.log(undefined>=0) //false
+
