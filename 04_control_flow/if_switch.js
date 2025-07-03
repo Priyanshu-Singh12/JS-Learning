c22647e8b7ba20ad13194bcb1887c5458844fa97
Priyanshu-Singh12 <progcoder34@gmail.com>
2025-07-03 15:50:34 +0530

    if switch done

---
+++ 04_control_flow/if_switch.js
@@ -0,0 +1,19 @@
+let balance=100
+//this is not good practice to have multi line separated by comma //!if(balance>500)console.log("Your balance is over than 500"),console.log("Hii")
+
+let key=10
+switch (key) {
+    case 10:
+        console.log("Hello")
+        break;
+        
+        
+    case 11:
+        console.log("Hello")
+        break;
+        
+        
+        default:
+        console.log("Hello")
+        break;
+}
