711ab165043db37c88a55d57507c2d54ad8efa0f
Priyanshu-Singh12 <progcoder34@gmail.com>
2025-07-03 20:15:44 +0530

    nothing in the new in loop

---
+++ 05_iterations/01_for_and_while.js
@@ -0,0 +1,17 @@
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    console.log(i)
+    
+}
+
+//shortcut to take cursor to all same word  use //! Ctrl+D
+/*while (condition) {
+    
+}*/
+
+let score=11
+do{
+    console.log(`Score is ${score}`)
+    score++
+
+}while(score<=10)
